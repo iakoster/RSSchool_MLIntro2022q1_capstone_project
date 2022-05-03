dc45dd2ed87636d0e0a4d570cf4f91bcfd10a3c6
Ivan Kosternoi <ivan.kosternoy@gmail.com>
2022-05-03 20:25:55 +0300

    feat: implement supports for command line arguments (input dataset and output directory) in data profiling

--- src/rsschool_mlintro2022q1_capstone_project/data_profiling.py
+++ src/rsschool_mlintro2022q1_capstone_project/data_profiling.py
@@ -1,16 +1,33 @@
 from pathlib import Path
 
+import click
 import pandas as pd
+import pandas_profiling  # If you do not import this,
+# that .profile_report method of pandas dataframe does not work
 
-
-def profile_data():
-    dataset_path = Path() / r'data\train.csv'
-    output_dir = Path() / r'data\profiles'
-
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-o",
+    "--output_dir",
+    default="data/profiles",
+    type=click.Path(file_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+def profile_data(
+        dataset_path: Path,
+        output_dir: Path
+):
     if dataset_path.suffix != '.csv':
         raise TypeError(
             'Data profiling does not support '
-            'anything other than .csv data type')
+            'anything other than .csv data extension')
     if not output_dir.exists():
         output_dir.mkdir(parents=True)
 
