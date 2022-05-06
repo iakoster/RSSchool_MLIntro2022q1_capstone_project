fabc630d773d8733ae9c9e89dc7ee54d2fabbbf9
Ivan Kosternoi <ivan.kosternoy@gmail.com>
2022-05-06 21:27:37 +0300

    feat: attach console commands to the .train method

--- src/rsschool_mlintro2022q1_capstone_project/models.py
+++ src/rsschool_mlintro2022q1_capstone_project/models.py
@@ -4,7 +4,18 @@ from sklearn.pipeline import Pipeline
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def make_pipeline() -> Pipeline:
-    return Pipeline([
-        ('knn', KNeighborsClassifier())
-    ])
+def create_pipeline(
+        model: str = 'knn',
+        random_state: int = 42,
+        knn_neighbors: int = 5,
+) -> Pipeline:
+    assert model in (
+        'knn',
+    ), 'invalid model: %s' % model
+
+    pipeline_steps = []
+    if model == 'knn':
+        pipeline_steps.append(('knn', KNeighborsClassifier(
+            n_neighbors=knn_neighbors
+        )))
+    return Pipeline(steps=pipeline_steps)
--- src/rsschool_mlintro2022q1_capstone_project/train.py
+++ src/rsschool_mlintro2022q1_capstone_project/train.py
@@ -6,11 +6,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-from .models import make_pipeline
+from .models import create_pipeline
 
 
-def get_dataset(dataset_path: Path
-                ) -> tuple[pd.DataFrame, pd.Series]:
+def get_dataset_xy(dataset_path: Path
+                   ) -> tuple[pd.DataFrame, pd.Series]:
     if dataset_path.suffix != '.csv':
         raise TypeError(
             'Data profiling does not support '
@@ -22,16 +22,55 @@ def get_dataset(dataset_path: Path
 
 
 @click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-m",
+    "--model",
+    default="knn",
+    type=str,
+    show_default=True,
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+)
+@click.option(
+    "--knn-neighbors",
+    default=5,
+    type=click.IntRange(0, min_open=True),
+    show_default=True,
+)
 def train(
         dataset_path: Path = None,
         model: str = 'knn',
-        random_state: int = 42
+        random_state: int = 42,
+        test_split_ratio: float = 0.2,
+        knn_neighbors: int = 5,
 ):
     if dataset_path is None:
         dataset_path = Path(r'data\train.csv')
-    X, y = get_dataset(dataset_path)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=random_state)
-    model = make_pipeline()
+        *get_dataset_xy(dataset_path),
+        test_size=test_split_ratio,
+        random_state=random_state)
+    model = create_pipeline(
+        model=model,
+        random_state=42,
+        knn_neighbors=knn_neighbors,
+    )
     model.fit(X_train, y_train)
     print(accuracy_score(y_test, model.predict(X_test)))
