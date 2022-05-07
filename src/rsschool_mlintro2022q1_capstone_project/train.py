b6c45288c0f9da596544f8dd1fd2e1f35690bb69
Ivan Kosternoi <ivan.kosternoy@gmail.com>
2022-05-07 20:29:09 +0300

    feat: add supporting of KFold in model training

--- src/rsschool_mlintro2022q1_capstone_project/train.py
+++ src/rsschool_mlintro2022q1_capstone_project/train.py
@@ -2,9 +2,10 @@ from pathlib import Path
 from joblib import dump
 
 import click
+import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import accuracy_score
 
 from .models import create_pipeline, get_metrics
@@ -24,8 +25,9 @@ def get_dataset(
     return df
 
 
-def get_dataset_xy(dataset_path: Path
-                   ) -> tuple[pd.DataFrame, pd.Series]:
+def get_dataset_xy(
+        dataset_path: Path
+) -> tuple[pd.DataFrame, pd.Series]:
     df = get_dataset(dataset_path)
     return df.drop(columns='cover_type'), df['cover_type']
 
@@ -59,9 +61,15 @@ def get_dataset_xy(dataset_path: Path
     show_default=True,
 )
 @click.option(
-    "--test-split-ratio",
-    default=0.2,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    "--k-folds",
+    default=5,
+    type=click.IntRange(1, min_open=True),
+    show_default=True,
+)
+@click.option(
+    "--shuffle-folds",
+    default=False,
+    type=bool,
     show_default=True,
 )
 @click.option(
@@ -75,25 +83,47 @@ def train(
         save_model_path: Path,
         model: str,
         random_state: int,
-        test_split_ratio: float,
+        k_folds: int,
+        shuffle_folds: bool,
         knn_neighbors: int,
 ):
-    X_train, X_test, y_train, y_test = train_test_split(
-        *get_dataset_xy(dataset_path),
-        test_size=test_split_ratio,
-        random_state=random_state
-    )
+    X, y = get_dataset_xy(dataset_path)
     pipeline = create_pipeline(
         model=model,
         random_state=random_state,
         knn_neighbors=knn_neighbors,
     )
-    pipeline.fit(X_train, y_train)
-    accuracy, f1, precision = get_metrics(
-        y_test, pipeline.predict(X_test))
+    kf = KFold(n_splits=k_folds,
+               shuffle=shuffle_folds,
+               random_state=random_state
+               if shuffle_folds else None)
+
+    accuracy_folds, f1_folds, precision_folds = [], [], []
+    accuracy_best, f1_best, precision_best = 0, 0, 0
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        pipeline.fit(X_train, y_train)
+        accuracy, f1, precision = get_metrics(
+            y_test, pipeline.predict(X_test))
+        accuracy_folds.append(accuracy)
+        f1_folds.append(f1)
+        precision_folds.append(precision)
+        if accuracy > accuracy_best:
+            accuracy_best, f1_best, precision_best = \
+                accuracy, f1, precision
+            dump(pipeline, save_model_path)
     click.echo(
-        f'Accuracy: {accuracy}, '
-        f'F1 score: {f1}, '
-        f'Precision: {precision}'
+        f'Mean metrics. '
+        f'Accuracy: {np.mean(accuracy_folds):.6f}, '
+        f'F1 score: {np.mean(f1_folds):.6f}, '
+        f'Precision: {np.mean(precision_folds):.6f}'
     )
-    dump(pipeline, save_model_path)
+    click.echo(
+        f'Best metrics. '
+        f'Accuracy: {accuracy_best:.6f}, '
+        f'F1 score: {f1_best:.6f}, '
+        f'Precision: {precision_best:.6f}'
+    )
+    click.echo(f'Best model saved in {save_model_path}')
+
